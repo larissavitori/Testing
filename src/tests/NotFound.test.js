d4826e5806a97b2ceb283e6e7e277fdaa818b057
larissa <laryterra@gmail.com>
2023-01-21 01:01:05 -0300

    reeq3

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,10 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from './renderWithRouter';
+import { FavoritePokemon } from '../pages';
+
+test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+  renderWithRouter(<FavoritePokemon />);
+  const poke = screen.getByText(/No favorite Pokémon found/i);
+  expect(poke).toBeInTheDocument();
+});
